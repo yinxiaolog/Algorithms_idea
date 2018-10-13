18556961444d9cadb619dcdf02fdfbc619fa9c45
yinxiaolong <15121849303@163.com>
2018-10-13 22:14:02 +0800

    Initial commit

---
+++ src/chapter2/Exercise1203.java
@@ -0,0 +1,7 @@
+package chapter2;
+
+public class Exercise1203 {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/chapter2/Exercise1205.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise1205 {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        s.toUpperCase();
+        s.substring(6, 11);
+        StdOut.println(s);
+    }
+}
